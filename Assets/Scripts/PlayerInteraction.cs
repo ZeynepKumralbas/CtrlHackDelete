a3270a91fba03fa15e0410d4552011b115835351
Eymen179 <eymenarapoglu5454@gmail.com>
2025-06-30 16:56:38 +0300

    Added simple visuals and simple mission difficulty

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,83 +1,155 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private InputActionReference interaction;
     [SerializeField] private float interactionTime = 10f;
+    [SerializeField] private GameObject txtInteractionButton;
+    [SerializeField] private Slider missionCompletePercentSlider;
 
     private Animator _animator;
+
     private bool isInMissionPoint = false;
     private bool isHolding = false;
+
     private Coroutine holdCoroutine;
+
     private GameObject currentMissionPoint;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+
+        if(txtInteractionButton != null) 
+            txtInteractionButton.SetActive(false);
+
+        if (missionCompletePercentSlider != null)
+        {
+            missionCompletePercentSlider.gameObject.SetActive(false);
+            missionCompletePercentSlider.maxValue = interactionTime;
+            missionCompletePercentSlider.value = interactionTime;
+        }
+
     }
     void Update()
     {
-        if (!isInMissionPoint) return;
+        if (!isInMissionPoint) return; //Görev alanýnda deðilse kod çalýþmaz
 
-        if (interaction.action.WasPressedThisFrame())
+        if (interaction.action.WasPressedThisFrame()) //Etkileþim tuþuna basýldýðý anda Corotine'i baþlat
         {
             holdCoroutine = StartCoroutine(HoldInteraction());
         }
 
-        if (interaction.action.WasReleasedThisFrame())
+        if (interaction.action.WasReleasedThisFrame()) //Etkileþim tuþuna basma býrakýldýðý anda Corotine'i ve animasyonu durdur
         {
             if (holdCoroutine != null)
             {
                 StopCoroutine(holdCoroutine);
                 holdCoroutine = null;
-                _animator.SetBool("isInteracting", false); // animasyonu durdur
+                _animator.SetBool("isInteracting", false);
                 Debug.Log("Tuþ erken býrakýldý, iþlem iptal.");
             }
         }
     }
 
-    private IEnumerator HoldInteraction()
+    private IEnumerator HoldInteraction() //Tuþa basýldýðý süre boyunca Coroutine ve animasyon çalýþýr
     {
         isHolding = true;
-        _animator.SetBool("isInteracting", true); // animasyonu baþlat
+        _animator.SetBool("isInteracting", true);
         float holdTime = 0f;
 
-        while (holdTime < interactionTime)
+        if(missionCompletePercentSlider != null)
         {
-            if (!interaction.action.IsPressed())
+            missionCompletePercentSlider.gameObject.SetActive(true);
+            missionCompletePercentSlider.value = interactionTime;
+        }
+
+        while (holdTime < interactionTime) //Tuþa ilgili süre kadar basýlmasý istenir
+        {
+            if (!interaction.action.IsPressed()) //Tuþ býrakýldýðýnda animasyon durur ve Coroutine'den çýkýlýr
             {
-                _animator.SetBool("isInteracting", false); // animasyonu durdur
-                yield break; // tuþ býrakýldýysa çýk
+                _animator.SetBool("isInteracting", false);
+
+                if (missionCompletePercentSlider != null)
+                {
+                    missionCompletePercentSlider.gameObject.SetActive(true);
+                    missionCompletePercentSlider.value = interactionTime;
+                }
+
+                yield break;
             }
 
             holdTime += Time.deltaTime;
+
+            if (missionCompletePercentSlider != null)
+            {
+                missionCompletePercentSlider.value = interactionTime - holdTime;
+            }
+
             yield return null;
         }
 
-        // Görev tamamlandý
+        //Ýlgili süre kadar basýlýrsa görev tamamlanýr, Coroutine ve animasyon durdurulur
         Debug.Log("Görev tamamlandý!");
         _animator.SetBool("isInteracting", false);
         isHolding = false;
 
-        if (currentMissionPoint != null)
+        if (missionCompletePercentSlider != null)
         {
-            currentMissionPoint.SetActive(false); // görev alanýný kapat
+            missionCompletePercentSlider.gameObject.SetActive(false);
+            missionCompletePercentSlider.value = 0;
         }
+
+        if (currentMissionPoint != null) //Görev alaný kapatýlýr
+        {
+            currentMissionPoint.SetActive(false);
+
+            if (txtInteractionButton != null)
+                txtInteractionButton.SetActive(false);
+        }
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //Görev alanýna giriþ kontrolcüsü
     {
         if (other.CompareTag("MissionPoint"))
         {
             isInMissionPoint = true;
             currentMissionPoint = other.gameObject;
+
+            switch (currentMissionPoint.transform.GetChild(0).tag)
+            {
+                case "EasyMission":
+                    interactionTime = interactionTime* 1;
+                    missionCompletePercentSlider.maxValue = interactionTime;
+                    break;
+                case "NormalMission":
+                    interactionTime = interactionTime * 2;
+                    missionCompletePercentSlider.maxValue = interactionTime;
+                    break;
+                case "HardMission":
+                    interactionTime = interactionTime * 3;
+                    missionCompletePercentSlider.maxValue = interactionTime;
+                    break;
+                default:
+                    break;
+            }
+
+            if (txtInteractionButton != null)
+            {
+                txtInteractionButton.transform.position = currentMissionPoint.transform.position + new Vector3(0f, 1.5f, 0f);  
+                txtInteractionButton.SetActive(true);
+            }
+
             Debug.Log("Görev alanýna girildi");
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other) //Görev alanýndan çýkýþ kontrolcüsü
     {
         if (other.CompareTag("MissionPoint"))
         {
@@ -89,8 +161,19 @@ public class PlayerInteraction : MonoBehaviour
                 StopCoroutine(holdCoroutine);
                 holdCoroutine = null;
                 _animator.SetBool("isInteracting", false);
-                Debug.Log("Görev alaný terk edildi, iþlem iptal.");
             }
+
+            if (missionCompletePercentSlider != null)
+            {
+                missionCompletePercentSlider.gameObject.SetActive(false);
+                missionCompletePercentSlider.value = interactionTime;
+            }
+
+            if (txtInteractionButton != null)
+                txtInteractionButton.SetActive(false);
+
+            Debug.Log("Görev alaný terk edildi, iþlem iptal.");
+
         }
     }
 }
