598c028951d8ad6c2afc75381d181a3617f155c7
Eymen179 <eymenarapoglu5454@gmail.com>
2025-07-24 23:13:16 +0300

    revert merge

--- Assets/Scripts/MenuUI/MenuButton.cs
+++ Assets/Scripts/MenuUI/MenuButton.cs
@@ -1,53 +1,15 @@
-using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UI;
+using DG.Tweening;
 
 public class MenuButton : MonoBehaviour
 {
-    private Vector3 originalScale;
-    private Image buttonImage;
-    private Color originalColor;
-    public Color hoverColor = Color.red;
-
-    // Hangi butonlarda renk deðiþecekse buraya ekle
-    private readonly string[] colorChangeButtons = { "LeaveRoomButton", "LeaveGameButton", "QuitGameButton" };
-
-    void Start()
-    {
-        originalScale = transform.localScale;
-
-        buttonImage = GetComponent<Image>();
-        if (buttonImage != null)
-            originalColor = buttonImage.color;
-    }
-
     public void OnHover()
     {
-        transform.DOScale(originalScale * 1.1f, 0.2f).SetEase(Ease.OutBack);
-
-        if (ShouldChangeColor() && buttonImage != null)
-        {
-            buttonImage.DOColor(hoverColor, 0.2f);
-        }
+        transform.DOScale(1.1f, 0.2f).SetEase(Ease.OutBack);
     }
 
     public void OnExit()
     {
-        transform.DOScale(originalScale, 0.2f).SetEase(Ease.InBack);
-
-        if (ShouldChangeColor() && buttonImage != null)
-        {
-            buttonImage.DOColor(originalColor, 0.2f);
-        }
-    }
-
-    private bool ShouldChangeColor()
-    {
-        foreach (var name in colorChangeButtons)
-        {
-            if (gameObject.name == name)
-                return true;
-        }
-        return false;
+        transform.DOScale(1f, 0.2f).SetEase(Ease.InBack);
     }
 }
